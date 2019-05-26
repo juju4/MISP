29523cc00c87b280c64db20d4224bbd1e819d658
juju4 <juju4@users.noreply.github.com>
2019-05-26 08:56:31 -0400

    strict typing - snuffleupagus tests

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -663,7 +663,7 @@ class AppController extends Controller
             foreach ($options['paramArray'] as $p) {
                 if (
                     isset($options['ordered_url_params'][$p]) &&
-                    (!in_array(strtolower($options['ordered_url_params'][$p]), array('null', '0', false, 'false', null)))
+                    (!in_array(strtolower((string)$options['ordered_url_params'][$p]), array('null', '0', false, 'false', null)))
                 ) {
                     $data[$p] = $options['ordered_url_params'][$p];
                     $data[$p] = str_replace(';', ':', $data[$p]);
